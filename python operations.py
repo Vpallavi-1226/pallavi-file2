c34342ad300bb39efec045c006ebe4ac4e385446
Vpallavi-1226 <pallavivadiganeni13@gmail.com>
2026-07-20 21:18:49 +0530

    Add files via upload

---
+++ python operations.py
@@ -0,0 +1,19 @@
+a = 3
+b = 2
+c = 2
+print(a**b)
+print(a**c)
+print(a**b**c)
+x = 2
+print(x)
+x += 3
+print(x)
+a = 5
+b = 3
+print(a < b or a > b)
+print(not(a<b))
+print(not(a>b))
+a = 1
+b = 2
+print(a < b and a > b)
+
